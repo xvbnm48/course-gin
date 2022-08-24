08ba8dcc2b5d2588d9623878fb4378008ac85a1a
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-08-24 22:50:14 +0700

    binding data

--- main.go
+++ main.go
@@ -8,36 +8,72 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type urlBinding struct {
+	ID string `uri:"id"`
+}
+
+type Products struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type ProductsShouldBind struct {
+	ID   string `form:"id"`
+	Name string `form:"name"`
+}
+
+type headerBinding struct {
+	RequestId string `header:"x-request-id"`
+}
+
 func main() {
 	router := gin.Default()
 	address := ":3000"
-	v1 := router.Group("/api/v1")
 
-	v1.GET("/hello", func(c *gin.Context) {
-		c.String(200, "hello world")
+	router.GET("/products/:id", func(ctx *gin.Context) {
+		var binding urlBinding
+		if e := ctx.ShouldBindUri(&binding); e != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": e.Error()})
+			return
+		}
+
+		fmt.Println("the id is ", binding.ID)
+		ctx.String(http.StatusOK, "the id is %s", binding.ID)
+	})
+	// should bind json
+	router.POST("/products/", func(ctx *gin.Context) {
+		var bindJSon Products
+
+		if err := ctx.ShouldBindJSON(&bindJSon); err != nil {
+			ctx.String(http.StatusBadRequest, "the data is not json")
+			return
+		}
+
+		fmt.Println("Product:", bindJSon)
+		ctx.String(http.StatusOK, "the id is %s", bindJSon.ID)
+	})
+	// shoud bind
+	router.POST("/products2", func(ctx *gin.Context) {
+		var products2 ProductsShouldBind
+		if err := ctx.ShouldBind(&products2); err != nil {
+			ctx.String(http.StatusBadRequest, "the data is not json")
+			return
+		}
+
+		fmt.Println("Product:", products2)
+		ctx.String(http.StatusOK, "the id is %s", products2.ID)
 	})
 
-	v2 := router.Group("/api/v2")
-	v2.GET("/get", func(c *gin.Context) {
-		c.String(http.StatusOK, "get")
+	router.POST("/header", func(ctx *gin.Context) {
+		binding := headerBinding{}
+		if err := ctx.ShouldBindHeader(&binding); err != nil {
+			ctx.String(http.StatusBadRequest, err.Error())
+			return
+		}
+		fmt.Println("the request id is ", binding.RequestId)
+		ctx.String(http.StatusOK, "the request id is %s", binding.RequestId)
 	})
 
-	router.GET("/:id", func(c *gin.Context) {
-		id := c.Param("id")
-		c.String(http.StatusOK, "The id is %s", id)
-	})
-
-	router.POST("/path", func(c *gin.Context) {
-		id := c.DefaultQuery("id", "11")
-		fmt.Println("id:", id)
-		c.String(http.StatusOK, "The id is %s", id)
-	})
-
-	router.POST("/products/:id", func(c *gin.Context) {
-		id := c.GetHeader("id")
-		fmt.Println("id:", id)
-		c.String(http.StatusOK, "The id is %s", id)
-	})
 	log.Fatal(router.Run(address))
 
 }
