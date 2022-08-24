ad49082e3313bd814cf81c42c77828bdcb0f0e0f
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-08-24 22:02:57 +0700

    learn route group

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,9 +10,15 @@ import (
 func main() {
 	router := gin.Default()
 	address := ":3000"
+	v1 := router.Group("/api/v1")
 
-	router.GET("/hello", func(c *gin.Context) {
+	v1.GET("/hello", func(c *gin.Context) {
 		c.String(200, "hello world")
+	})
+
+	v2 := router.Group("/api/v2")
+	v2.GET("/get", func(c *gin.Context) {
+		c.String(http.StatusOK, "get")
 	})
 
 	log.Fatal(router.Run(address))
