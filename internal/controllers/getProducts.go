67b6c17643e9b5d4a5dbfc0ad4038af662a3fbb7
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-08-26 21:10:02 +0700

    completed function getproducts and fix error return not all products

--- internal/controllers/getProducts.go
+++ internal/controllers/getProducts.go
@@ -4,10 +4,39 @@ import (
 	"database/sql"
 
 	"github.com/gin-gonic/gin"
+	"github.com/xvbnm48/course-gin/internal"
 )
 
 func GetProducts(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.String(200, "GetProducts")
+		var rows *sql.Rows
+		var e error
+		if rows, e = db.Query("SELECT guid,name,price,description,createdAt FROM products"); e != nil {
+			var res = internal.NewHttpResponse(500, e)
+			c.JSON(500, res)
+			return
+		}
+		defer rows.Close()
+		var products []Product
+		for rows.Next() {
+			var product Product
+
+			if e := rows.Scan(&product.GUID, &product.Name, &product.Price, &product.Description, &product.CreatedAt); e != nil {
+				var res = internal.NewHttpResponse(500, e)
+				c.JSON(500, res)
+				return
+			}
+
+			products = append(products, product)
+		}
+
+		if len(products) == 0 {
+			var res = internal.NewHttpResponse(404, sql.ErrNoRows)
+			c.JSON(404, res)
+			return
+		}
+
+		var res = internal.NewHttpResponse(200, products)
+		c.JSON(200, res)
 	}
 }
